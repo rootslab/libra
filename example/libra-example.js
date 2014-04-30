ebeb5113e48ee03be9d7ea72747fe6eacae734b2
rootslab <44gatti@gmail.com>
2014-04-30 14:33:40 +0200

    Hello Folks! On branch master Initial commit new file:   .npmignore new file:   .travis.yml new file:   LICENSE new file:   Readme.md new file:   example/libra-example.js new file:   index.js new file:   lib/libra.js new file:   package.json new file:   test/empty-test.js new file:   test/run.sh

---
+++ example/libra-example.js
@@ -0,0 +1,9 @@
+/* 
+ * Libra Example
+ */
+
+var log = console.log
+    , util = require( 'util' )
+    , Libra = require( '../' )
+    , l = Libra()
+    ;
---
+++ test/empty-test.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/* 
+ * Empty Test
+ */
+
+var log = console.log
+    , assert = require( 'assert' )
+    , util = require( 'util' )
+    , Libra = require( '../' )
+    , l = Libra()
+    ;
