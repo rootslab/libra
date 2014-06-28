2701f45ddad87556d4ee8d9e45f21aa8a2106e19
rootslab <44gatti@gmail.com>
2014-06-28 17:34:47 +0200

    v0.9.1. Added tests for SELECT and AUTH. On branch master modified:   test/auth-mode-test.js new file:   test/select-mode-test.js

--- test/select-mode-test.js
+++ test/select-mode-test.js
@@ -1,5 +1,5 @@
 /* 
- * Libra AUTH mode
+ * Libra SELECT mode
  */
 
 var log = console.log
@@ -11,8 +11,10 @@ var log = console.log
     , l = Libra()
     , syl = Syllabus()
     , auth = syl.commands.auth
+    , select = syl.commands.select
     , ping = syl.commands.ping
     , acmd = auth( 'foobared', emptyFn )
+    , scmd = select( 0, emptyFn )
     , pcmd = ping()
     ;
 
@@ -22,11 +24,23 @@ l.push( pcmd );
 log( '- call Libra#auth with encoded AUTH command.' );
 l.auth( acmd );
 
+log( '- call Libra#select with encoded SELECT command.' );
+l.select( scmd );
+
 log( '- check if status property was correctly updated.' );
 assert.ok( l.status.auth === acmd );
+
+log( '- check if status property was correctly updated.' );
+assert.ok( l.status.select === scmd );
 
 log( '- call Libra#pop, result should be AUTH command and not PING.' );
 assert.ok( l.pop() === acmd );
 
 log( '- now Libra.status.auth should be resetted to null.' );
+assert.ok( l.status.auth === null );
+
+log( '- call Libra#pop, result should be SELECT command and not PING.' );
+assert.ok( l.pop() === scmd );
+
+log( '- now Libra.status.select should be resetted to null.' );
 assert.ok( l.status.auth === null );
